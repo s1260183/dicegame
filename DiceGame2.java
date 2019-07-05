a2077832fb14be10562e2a10bd28715ad7e8b7fb
s1260183 <s1260183@std5dc12.u-aizu.ac.jp>
2019-07-05 13:07:41 +0900

    Greetings added

---
+++ DiceGame2.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class DiceGame2{
+    public static void main(String[] args){
+	System.out.println("What is your name?");
+	Scanner scan = new Scanner(System.in);
+	String name = scan.next();
+	System.out.println("Hello," + name + "!");
+	
+	Random rnd = new Random();
+	System.out.println("Rolling the dice...");
+	int num1 = rnd.nextInt(6) + 1;
+	int num2 = rnd.nextInt(6) + 1;
+	System.out.println("Die 1:" + num1);
+	System.out.println("Die 2:" + num2);
+	System.out.println("Total value:" + (num1 + num2));	
+    }
+
+}
